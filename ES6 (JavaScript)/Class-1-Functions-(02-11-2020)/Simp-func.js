7fe32ca0614f99db114c1d14f29e1f60b3a54ca1
AbbasFaisalSsuet <abbas.faisal.ssuet@gmail.com>
2020-12-26 19:28:59 +0500

    CV and MarksSheet through "Simple Function" in JavaScript .....

---
+++ ES6 (JavaScript)/Class-1-Functions-(02-11-2020)/Simp-func.js
@@ -0,0 +1,344 @@
+    //Simple Function: (CV and MarkSheet)
+    //CV
+
+console.log("=====================================================");
+console.log("=============CV=============");
+console.log("=====================================================");
+
+function name()
+{
+    var name = "Faisal"
+    console.log(name);
+}
+
+name();
+
+function f_name()
+{
+    var f_name = "Karamat Hussain"
+    console.log(f_name);
+}
+
+f_name();
+
+function Mobile()
+{
+    var num = "0334035889"
+    console.log(num);
+}
+
+Mobile();
+
+function D_O_B()
+{
+    var DOB = "09-05-2000"
+    console.log(DOB);
+}
+
+D_O_B();
+
+function Email()
+{
+    var email = "faisalabbasjaffri05@gmail.com"
+    console.log(email);
+}
+
+Email();
+
+function Qualification()
+{
+    var quaf = "BSCS Semester III"
+    console.log(quaf);
+}
+
+Qualification();
+
+function Experience()
+{
+    var exp = "06 Months Data Entry Operator at Lepoard Courier"
+    console.log(exp);
+}
+
+Experience();
+
+function Address()
+{
+    var add = "House No.603/A Masoom Shah Colony kalapull Karachi"
+    console.log(add);
+}
+
+Address();
+
+function NIC_No()
+{
+    var Nic = "42301-4102229-9"
+    console.log(Nic);
+}
+
+NIC_No();
+
+function Religion()
+{
+    var Rel = "Islam"
+    console.log(Rel);
+}
+
+Religion();
+
+
+console.log("=====================================================");
+console.log("=============MARKS SHeeT=============");
+console.log("=====================================================");
+
+
+   //Marks Sheet
+
+function Maths()
+{
+    var mth = "Maths = 80"
+    console.log(mth);
+}
+
+Maths();
+
+function Computer()
+{
+    var Comp = "Computer = 95"
+    console.log(Comp);
+}
+
+Computer();
+
+function English()
+{
+    var eng = "English = 70"
+    console.log(eng);
+}
+
+English();
+
+function Programming_Fundamentals()
+{
+    var pmg_f = "Programming Fundamentals = 90"
+    console.log(pmg_f);
+}
+
+Programming_Fundamentals();
+
+function Probability()
+{
+    var Prb = "Pobability = 85"
+    console.log(Prb);
+}
+
+Probability();
+
+function Digital_Logic_Design()
+{
+    var Dld = "Digital Logic Design = 75"
+    console.log(Dld);
+}
+
+Digital_Logic_Design();
+
+function Discrete_Structure()
+{
+    var Dis = "Discrete Dtructure = 80"
+    console.log(Dis);
+}
+
+Discrete_Structure();
+
+function Total_Marks()
+{
+    var tom = "Obtained Marks/Toatal Marks = 575/700"
+    console.log(tom);
+}
+
+Total_Marks();
+
+function Grades()
+{
+    var grd = "Grade = A+"
+    console.log(grd);
+}
+
+Grades();
+
+function Percentage()
+{
+    var per = "Percentage = 82"
+    console.log(per);
+}
+
+Percentage();
+
+
+
+
+
+
+
+   //Return Funtion: (CV and MarkSheet)
+   //CV
+
+console.log("=====================================================");
+console.log("=============CV=============");
+console.log("=====================================================");
+
+function Name()
+{
+    return "Faisal Abbas"
+}
+var func1 = Name()
+console.log(func1);
+
+
+function F_Name()
+{
+    return "Karamat Hussain"
+}
+var func2 = F_Name()
+console.log(func2);
+
+
+function Mobile()
+{
+    return "03340358289"
+}
+var func3 = Mobile()
+console.log(func3);
+
+
+function DateofBirth()
+{
+    return "09-05-2000"
+}
+var func4 = DateofBirth()
+console.log(func4);
+
+
+function Email()
+{
+    return "faisalabbasjaffri05@gmail.com"
+}
+var func5 = Email()
+console.log(func5);
+
+
+function Qualification()
+{
+    return "BSCS Semester III"
+}
+var func6 = Qualification()
+console.log(func6);
+
+
+function Experience()
+{
+    return "06 Months Data Entry Operator at Lepoard Courier"
+}
+var func7 = Experience()
+console.log(func7);
+
+
+function Address()
+{
+    return "House No.603/A Masoom Shah Colony kalapull Karachi"
+}
+var func8 = Address()
+console.log(func8);
+
+
+function NIC_No()
+{
+    return "42301-4102229-9"
+}
+var func9 = NIC_No()
+console.log(func9);
+
+
+function Religion()
+{
+    return "Islam"
+}
+var func10 = Religion()
+console.log(func10);
+
+
+console.log("=====================================================");
+console.log("=============MARKS SHeeT=============");
+console.log("=====================================================");
+
+
+   //Marks Sheet
+
+function Maths()
+{
+    return "Maths = 80"
+}
+var mth1 = Maths()
+console.log(mth1);
+
+function Computer()
+{
+    return "Computer = 95"
+}
+var comp2 = Computer()
+console.log(comp2);
+
+function English()
+{
+    return "English = 70"
+}
+var eng3 = English()
+console.log(eng3);
+
+function Programming_Fundamentals()
+{
+    return "Programming_Fundamentals = 90"
+}
+var pgf4 = Programming_Fundamentals()
+console.log(pgf4);
+
+function Probability()
+{
+    return "Probability = 85"
+}
+var pby5 = Probability()
+console.log(pby5);
+
+function Digital_Logic_Design()
+{
+    return "Digital Logic Design = 75"
+}
+var dld6 = Digital_Logic_Design()
+console.log(dld6);
+
+function Discrete_Structure()
+{
+    return "Discrete_Structure = 80"
+}
+var dis7 = Discrete_Structure()
+console.log(dis7);
+
+function Total_Marks()
+{
+    return "Total_Marks = 575/700"
+}
+var tom8 = Total_Marks()
+console.log(tom8);
+
+function Grades()
+{
+    return "Grades= A+"
+}
+var grd9 = Grades()
+console.log(grd9);
+
+function Percentage()
+{
+    return "Percentage = 82%"
+}
+var per10 = Percentage()
+console.log(per10);
